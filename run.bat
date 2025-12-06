e29e1b0518fb9d6c181365e16f8e052173e04699
Silly_Gamer <basisty.marcin@gmail.com>
2025-12-06 15:04:31 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,11 +1,12 @@
 ::-------------------------------------------------
 :: Change the path to your SDL3 directory if needed
 ::-------------------------------------------------
-cmake -G "MinGW Makefiles" -S . -B dist -DSDL3_DIR=D:\Aplikacje\SDL3\SDL3-3.1.3\x86_64-w64-mingw32\lib\cmake\SDL3
+cmake -G "MinGW Makefiles" -S . -B dist -DSDL3_DIR=D:\example_path_to_SDL3_directory
 
 cd dist
 cmake --build .
 
 app
+
 
 cd ..
